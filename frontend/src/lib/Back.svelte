0c4a86f540f1b1f3504aae95ae6411fc46bf26f6
mtte <code@mtte.me>
2023-10-29 01:13:24 +0200

    Implement back component

---
+++ frontend/src/lib/Back.svelte
@@ -0,0 +1,12 @@
+<script>
+    function goBack() {
+        history.back();
+    }
+</script>
+
+<div class="bg-transparent absolute text-white top-16 left-8">
+    <!-- angle-left -->
+    <button on:click={goBack} class="btn w-8 h-8">
+        <i class="fa fa-angle-left text-2xl mx-auto"/>
+    </button>
+</div>
--- frontend/src/routes/events/past/+page.svelte
+++ frontend/src/routes/events/past/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Back from "$lib/Back.svelte";
+
     // Mock Data
     let events = [
         { id: 1, state: 'PAST', description: "Day one of coding", startDateTime: '28-10-2023 09:30', duration: 14, displayName: "Basel Hack 2023", ownerId: 1 },
@@ -22,24 +24,21 @@
 </script>
 
 <div class="flex-grow w-full bg-background px-4">
+    <Back />
     <div class="flex flex-col justify-center items-center py-14">
         <h1 class="text-white text-4xl">ReLive</h1>
         <p class="text-white">the moments from the past</p>
     </div>
     <div class="flex flex-col w-full">
         {#each events as event}
-        <div class="cursor-pointer w-full my-2 py-6 px-2 rounded-xl" style="background-color: #{getColor()}">
-            <p class="text-white text-xl">
-                {event.displayName}
-            </p>
-            <p class="text-white text-sm pt-1">
-                {event.startDateTime.split(' ')[0]}
-            </p>
-        </div>
-        
-    {/each}
-        <div class="flex flex-row my-2 overflow-x-scroll overflow-y-hidden">
-           
-        </div>
+            <div class="cursor-pointer w-full my-2 py-6 px-4 rounded-xl" style="background-color: #{getColor()}">
+                <p class="text-white text-xl">
+                    {event.displayName}
+                </p>
+                <p class="text-white text-sm pt-1">
+                    {event.startDateTime.split(' ')[0]}
+                </p>
+            </div>
+        {/each}
     </div>
 </div>
