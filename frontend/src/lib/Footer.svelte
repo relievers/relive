1cdaf060ea7240ce7aaf97a2cb13329e19b093fa
Noah Grun <noah.grun@students.bfh.ch>
2023-10-28 20:13:04 +0200

    Event grid

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -5,7 +5,7 @@
 <div class="bg-gray-900 text-white p-4">
     <div class="container mx-auto flex justify-between">
         <div class="w-1/3 text-center">
-            <a class="bg-transparent hover:text-gray-500 font-bold flex flex-col items-center" href="/#">
+            <a class="bg-transparent hover:text-gray-500 font-bold flex flex-col items-center" href="/events">
                 <i class="fa fa-calendar text-2xl mx-auto"></i>
                 <span class="text-xs">Events</span>
             </a>
---
+++ frontend/src/routes/events/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { each } from "svelte/internal";
+
+    // Mock Data
+    let events = [
+        { id: 1, state: 'PAST', description: "Day one of coding", startDateTime: '28-10-2023 09:30', duration: 14, displayName: "BaselHack 2023", ownerId: 1 },
+        { id: 2, state: 'ONGOING', description: "Hacking in Pratteln", startDateTime: '28-10-2023 09:30', duration: 36, displayName: "BaselHack 2023", ownerId: 1 },
+        { id: 3, state: 'PLANNED', description: "Hacking again in Pratteln", startDateTime: '28-10-2024 09:30', duration: 36, displayName: "BaselHack 2024", ownerId: 1 },
+        { id: 4, state: 'PLANNED', description: "Hacking again in Pratteln", startDateTime: '28-10-2025 09:30', duration: 36, displayName: "BaselHack 2025", ownerId: 1 },
+        { id: 5, state: 'PAST', description: "I love Techno", startDateTime: '25-05-2023 22:30', duration: 2, displayName: "Viertel Klub: Stella Bossi", ownerId: 1 }
+    ]
+</script>
+
+<div class="flex-grow w-full bg-red-300">
+    <div class="bg-gray-900 flex flex-col justify-center items-center">
+        <h1 class="text-white text-4xl">Events</h1>
+        <h2 class="text-white text-2xl"> Ongoing Event</h2>
+        {#each events as event}
+            {#if event.state === 'ONGOING'}
+                <div>
+                    <span class="text-white">
+                        {event.id}, {event.state}, {event.description}, {event.startDateType}, {event.duration}, {event.displayName}, {event.ownerId}
+                    </span>
+                </div>
+            {/if}
+        {/each}
+    </div>
+    <div class="bg-gray-900 flex flex-col justify-center items-center">
+        <h2 class="text-white text-2xl">Your Events</h2>
+        {#each events as event}
+            {#if event.state === 'PLANNED' && event.ownerId === 1} <!--TODO: OwnerID-->
+                <div>
+                    <span class="text-white">
+                        {event.id}, {event.state}, {event.description}, {event.startDateType}, {event.duration}, {event.displayName}, {event.ownerId}
+                    </span>
+                </div>
+            {/if}
+        {/each} 
+    </div>
+    <div class="bg-gray-900 flex flex-col justify-center items-center">
+        <h2 class="text-white text-2xl">ReLive your Memories</h2>
+        {#each events as event}
+            {#if event.state === 'PAST'} 
+                <div>
+                    <span class="text-white">
+                        {event.id}, {event.state}, {event.description}, {event.startDateType}, {event.duration}, {event.displayName}, {event.ownerId}
+                    </span>
+                </div>
+            {/if}
+        {/each}
+    </div>
+</div>
