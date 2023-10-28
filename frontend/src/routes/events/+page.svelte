0477222e2447862ebd07582c44e0e8f665ccf088
Noah Grun <noah.grun@students.bfh.ch>
2023-10-28 20:29:35 +0200

    Add coloring algorithmus for passed events

--- frontend/src/routes/events/+page.svelte
+++ frontend/src/routes/events/+page.svelte
@@ -8,7 +8,20 @@
         { id: 3, state: 'PLANNED', description: "Hacking again in Pratteln", startDateTime: '28-10-2024 09:30', duration: 36, displayName: "BaselHack 2024", ownerId: 1 },
         { id: 4, state: 'PLANNED', description: "Hacking again in Pratteln", startDateTime: '28-10-2025 09:30', duration: 36, displayName: "BaselHack 2025", ownerId: 1 },
         { id: 5, state: 'PAST', description: "I love Techno", startDateTime: '25-05-2023 22:30', duration: 2, displayName: "Viertel Klub: Stella Bossi", ownerId: 1 }
-    ]
+    ];
+
+    // Color codes for variation
+    let colors = ['FFA95A', 'A2EE67', '67A5EE', 'CB67EE', '5C3D95'];
+
+    // iterator for color variation
+    let i = 0;
+
+    function getColor() {
+        let color = colors[i];
+        i += 1;
+        i = i % colors.length;
+        return color;
+    }
 </script>
 
 <div class="flex-grow w-full bg-red-300">
@@ -41,11 +54,12 @@
         <h2 class="text-white text-2xl">ReLive your Memories</h2>
         {#each events as event}
             {#if event.state === 'PAST'} 
-                <div>
+                <div style="background-color: #{getColor()}">
                     <span class="text-white">
                         {event.id}, {event.state}, {event.description}, {event.startDateType}, {event.duration}, {event.displayName}, {event.ownerId}
                     </span>
                 </div>
+                
             {/if}
         {/each}
     </div>
