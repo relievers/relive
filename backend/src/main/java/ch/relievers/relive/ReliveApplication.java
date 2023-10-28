43c77ff2070fd484b529e963eb3b39e3077ee648
Aaron Ebnöther <me@aarone.name>
2023-10-28 12:53:03 +0200

    Init spring boot

---
+++ backend/src/main/java/ch/relievers/relive/ReliveApplication.java
@@ -0,0 +1,13 @@
+package ch.relievers.relive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/relievers/relive/ReliveApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.relievers.relive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
