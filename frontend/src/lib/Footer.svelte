9d6ecb06d77de9c4276a7ae5f0b744572618bc6e
Noah Grun <noah.grun@students.bfh.ch>
2023-10-28 16:38:52 +0200

    Add 2nd footer depending if there is an active event

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,3 +1,7 @@
+<script>
+    let user = { activeEvent: false };
+</script>
+
 <div class="bg-gray-900 text-white p-4">
     <div class="container mx-auto flex justify-between">
         <div class="w-1/3 text-center">
@@ -6,12 +10,22 @@
                 <div class="text-xs">Events</div> <!-- Kleinerer Text -->
             </button>
         </div>
+        {#if user.activeEvent}
         <div class="w-1/3 text-center">
             <button class="bg-transparent hover:text-gray-500 font-bold py-2 px-4 rounded-full">
                 <i class="fa fa-camera text-2xl mx-auto"></i>
                 <div class="text-xs">ReLive</div> <!-- Kleinerer Text -->
             </button>
         </div>
+        {:else}
+        <div class="w-1/3 text-center">
+            <button class="bg-transparent hover:text-gray-500 font-bold py-2 px-4 rounded-full">
+                <i class="fa fa-qrcode text-2xl mx-auto"></i>
+                <div class="text-xs">ReLive</div> <!-- Kleinerer Text -->
+            </button>
+        </div>
+        {/if}
+        
         <div class="w-1/3 text-center">
             <button class="bg-transparent hover:text-gray-500 font-bold py-2 px-4 rounded-full">
                 <i class="fa fa-user-circle-o text-2xl mx-auto"></i>
