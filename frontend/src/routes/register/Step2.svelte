cf570e490cd576e0682a381ba8d5c818422b0855
Noah Salvi <46045829+noahsalvi@users.noreply.github.com>
2023-10-29 13:52:31 +0100

    Add join event site

---
+++ frontend/src/routes/register/Step2.svelte
@@ -0,0 +1,8 @@
+<h1 class="text-4xl">Step 2</h1>
+
+<div class="flex flex-col gap-10">
+	<h2 class="text-accent text-3xl">What should we call you?</h2>
+	<input class="bg-background w-full border-b-2 border-accent text-2xl placeholder:text-accent" placeholder="Your Name..." />
+</div>
+
+<div />
