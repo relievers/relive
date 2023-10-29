3529c4741b7276d88b4fee1672c4469ffdb31966
Noah Salvi <46045829+noahsalvi@users.noreply.github.com>
2023-10-29 13:45:59 +0100

    Add api

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,11 +1,21 @@
 const DATABASE_URL = 'http://localhost:8080/';
 
 function get(path: string) {
-	return fetch(DATABASE_URL + path, { method: 'GET' });
+	return fetch(DATABASE_URL + path, { method: 'GET' }).then((res) => {
+		if (!res.ok) {
+			throw res;
+		}
+		return res;
+	});
 }
 
 function post(path: string, body) {
-	return fetch(DATABASE_URL + path, { method: 'GET', body: body });
+	return fetch(DATABASE_URL + path, { method: 'POST', body: body }).then((res) => {
+		if (!res.ok) {
+			throw res;
+		}
+		return res;
+	});
 }
 
 export default { get, post };
